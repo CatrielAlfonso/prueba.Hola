5769d7c1814bdd7993b9c1491acfc17791e148b5
CatrielAlfonso <catrielalfonso77@gmail.com>
2022-06-13 07:27:24 -0300

    Se agrega el proyecto

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    int codigo;
+    char marca[20];
+    char modelo[20];
+    int capacidad;
+
+}ePendrive;
+
+/** \brief constructor que me consigue espacio en memoria e inicializa los campos en cero
+ *
+ * \return ePendrive* puntero a ePendrive
+ *
+ */
+ePendrive* new_pendrive();
+
+int main()
+{
+    int tam=5;
+    int* vectorDinamico
+
+
+    return 0;
+}
+
+ePendrive* new_pendrive()
+{
+
+     ePendrive* nuevoPendrive = (ePendrive*) calloc(1,sizeof(ePendrive));
+
+     return nuevoPendrive;
+}
